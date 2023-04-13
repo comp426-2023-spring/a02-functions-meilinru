c3aa3f415118b6dcde57ef78549a0d57c6dd2a0d
meilinru <77815163+meilinru@users.noreply.github.com>
2023-04-13 18:50:21 -0400

    trying to succeed

--- cli.js
+++ cli.js
@@ -9,7 +9,7 @@ async function main() {
 	if (args.h) {
 		console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE");
 		console.log("-h            Show this help message and exit.");
-		console.log("-n, -s        Latitude: N positive; oijS negative.");
+		console.log("-n, -s        Latitude: N positive; S negative.");
 		console.log("-e, -w        Longitude: E positive; W negative.");
 		console.log("-z            Time zone: uses tz.guess() from moment-timezone by default.");
 		console.log("-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.");
@@ -27,7 +27,7 @@ async function main() {
 	if (args.e) {
 		longitude = args.e.toFixed(2);
 	} else if (args.w) {
-		latitude = (-1 * args.w).toFixed(2);
+		longitude = (-1 * args.w).toFixed(2);
 	}	
 
 	const timezone = (args.z || moment.tz.guess());
