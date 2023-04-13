565253f371636f6d9c57c29eadd7d719f409568f
meilinru <77815163+meilinru@users.noreply.github.com>
2023-04-13 17:58:54 -0400

    created response text using conditional statements

--- cli.js
+++ cli.js
@@ -13,6 +13,33 @@ console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME
     console.log("-j            Echo pretty JSON from open-meteo API and exit.");
 }
 const timezone = moment.tz.guess();
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=35.91&longitude=-79.06&daily=precipitation_hours&timezone=America%2FNew_York');
+if (args.w) {
+    var latitude = (args.w * -1).toFixed(2);
+} else {
+    var latitude = args.e.toFixed(2);
+}
+if (args.s) {
+    var longitude = (args.s * -1).toFixed(2);
+} else {
+    var longitude = args.n.toFixed(2);
+}
+
+const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=precipitation_hours&timezone=America%2FNew_York`);
 const data = await response.json();
-console.log(data.daily.precipitation_hours[0]);
+
+const days = args.d;
+const rain = data.daily.precipitation_hours[days];
+
+if (rain > 0) {
+    process.stdout.write ("You might need your galoshes ");
+} else {
+    process.stdout.write ("You will not need your galoshes ");
+}
+
+if (days == 0) {
+    console.log ("today.");
+} else if (days == 1) {
+    console.log ("tomorrow.");
+} else {
+    console.log (`in ${days} days.`);
+}
