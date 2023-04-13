ac258c82984b2165daa34ad2bd211120ed68008b
meilinru <77815163+meilinru@users.noreply.github.com>
2023-04-13 18:12:21 -0400

    adding -j flag function

--- cli.js
+++ cli.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 import minimist from 'minimist';
-import moment from 'moment-timezone';
-import fetch from 'node-fetch';
+
+
 const args = minimist(process.argv.slice(2));
+
 if (args.h == true) {
 console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE");
     console.log("-h            Show this help message and exit.");
@@ -12,7 +13,16 @@ console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME
     console.log("-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.");
     console.log("-j            Echo pretty JSON from open-meteo API and exit.");
 }
+import moment from 'moment-timezone';
 const timezone = moment.tz.guess();
+
+import fetch from 'node-fetch';
+const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=precipitation_hours&timezone=${timezone}');
+const data = await response.json();
+
+if (args.j( {
+    console.log(data);
+}
 if (args.w) {
     var latitude = (args.w * -1).toFixed(2);
 } else {
@@ -24,8 +34,8 @@ if (args.s) {
     var longitude = args.n.toFixed(2);
 }
 
-const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=precipitation_hours&timezone=America%2FNew_York`);
-const data = await response.json();
+
+
 
 const days = args.d;
 const rain = data.daily.precipitation_hours[days];
