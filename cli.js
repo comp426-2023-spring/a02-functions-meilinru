ec18f72b2df6c9bba75e171d8cb299fc2f55c0ba
meilinru <77815163+meilinru@users.noreply.github.com>
2023-04-13 18:48:02 -0400

    changing lat and long to always to be defined as number

--- cli.js
+++ cli.js
@@ -9,7 +9,7 @@ async function main() {
 	if (args.h) {
 		console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE");
 		console.log("-h            Show this help message and exit.");
-		console.log("-n, -s        Latitude: N positive; S negative.");
+		console.log("-n, -s        Latitude: N positive; oijS negative.");
 		console.log("-e, -w        Longitude: E positive; W negative.");
 		console.log("-z            Time zone: uses tz.guess() from moment-timezone by default.");
 		console.log("-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.");
@@ -17,18 +17,18 @@ async function main() {
 		return 0;
 	}
 
-	var latitude;
-	var longitude;
-	if (args.w) {
-		latitude = (args.w * -1).toFixed(2);
-	} else if (args.e) {
-		latitude = args.e.toFixed(2);
-	}
-	if (args.s) {
-		longitude = (args.s * -1).toFixed(2);
-	} else if (args.n) {
-		longitude = args.n.toFixed(2);
-	}
+	var latitude = NaN;
+	if (args.n) {
+		latitude = args.n.toFixed(2);
+	} else if (args.s) {
+		latitude = (-1 * args.s).toFixed(2);
+	}	
+	var longitude=NaN;
+	if (args.e) {
+		longitude = args.e.toFixed(2);
+	} else if (args.w) {
+		latitude = (-1 * args.w).toFixed(2);
+	}	
 
 	const timezone = (args.z || moment.tz.guess());
 
